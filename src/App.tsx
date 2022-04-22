6eda56a5af804a62d33e29fd4ec111b56e5f4486
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2022-04-22 15:30:00 -0700

    Setup with graph and acs deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Stack } from '@fluentui/react';
+import { Login } from '@microsoft/mgt-react';
+import { _GraphToolkitEnabledProvider as GP, _useGraphToolkitEnabled, _useIsSignedIn } from '@azure/communication-react';
+
+const renderAppBody = () => {
+  const [isSignedIn] = _useIsSignedIn();
+
+  console.log('isSignedIn:', isSignedIn);
+
+  if (!isSignedIn) return <Login />;
+
+  return 'Invalid App Page.. Not sure how you got here.. file a github issue about this.';
+}
+
+// TODO FIX THIS!!!!
+if (window.localStorage.getItem('GraphUIToolkitEnabled') !== 'true'){
+  window.localStorage.setItem('GraphUIToolkitEnabled', 'true')
+  window.location.reload();
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GP isEnabled={true}>
+      <Stack verticalFill verticalAlign='center' horizontalAlign='center'>
+        {renderAppBody()}
+      </Stack>
+    </GP>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { Msal2Provider } from '@microsoft/mgt-msal2-provider';
+import { Providers } from '@microsoft/mgt-element';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+Providers.globalProvider = new Msal2Provider({
+  clientId: 'a2ee26d1-a1e2-4289-b37b-1da484a72fb8',
+  scopes: ['ChatMember.Read', 'ChatMember.ReadWrite', 'Chat.ReadBasic', 'Chat.Read', 'Chat.ReadWrite']
+});
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
